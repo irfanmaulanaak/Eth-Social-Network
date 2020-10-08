adafe3d5ac49422e9ce924161e22353eef51025a
irfan <irfanjamesway@yahoo.co.id>
2020-10-08 15:28:06 +0700

    boom

--- src/contracts/SocialNetwork.sol
+++ src/contracts/SocialNetwork.sol
@@ -9,16 +9,54 @@ contract SocialNetwork {
         uint id;
         string content;
         uint tipAmount;
-        address author;
+        address payable author;
     }
 
+    event PostCreated(
+        uint id,
+        string content,
+        uint tipAmount,
+        address payable author
+    );
+
+    event PostTipped(
+        uint id,
+        string content,
+        uint tipAmount,
+        address payable author
+    );
+
     constructor() public{
-        name = "Ethereum Social Network";
+        name = "Decentralized Social Network";
     }
+
+    //this is write function so we must pay gas
     function createPost(string memory _content) public {
+        //Require content so the value must not be null
+        require(bytes(_content).length > 0);
         // Increment post count
         postCount ++;
         //create post
         posts[postCount] = Post(postCount, _content, 0, msg.sender);
+        //Trigger event
+        emit PostCreated(postCount, _content, 0, msg.sender);
+
+    }
+    function tipPost(uint _id) public payable {
+        //make sure id is valid
+        require(_id > 0 && _id <= postCount);
+        //Fetch post
+        Post memory _post = posts[_id];
+        //Fetch author
+        address payable _author = _post.author;
+        //pay author
+        address(_author).transfer(msg.value);
+        //Increment tip amount
+        // 1 ether = 1000000000000000000 wei
+        _post.tipAmount = _post.tipAmount + msg.value;
+        //update post
+        posts[_id] = _post;
+        //trigger event
+        emit PostTipped(postCount, _post.content, _post.tipAmount, _author);
     }
 }
--- test/SocialNetwork.js
+++ test/SocialNetwork.js
@@ -1,45 +1,96 @@
 const { assert } = require('chai')
+const { default: Web3 } = require('web3')
 
 const SocialNetwork = artifacts.require('./SocialNetwork.sol')
+
 require('chai')
-    .use(require('chai-as-promised'))
-    .should()
+  .use(require('chai-as-promised'))
+  .should()
 
 contract('SocialNetwork', ([deployer, author, tipper]) => {
-    let socialNetwork
+  let socialNetwork
 
-    before(async () =>{
-        socialNetwork = await SocialNetwork.deployed()
+  //before hooks for the deployment
+  before(async () => {
+      //runs once before the first block
+    socialNetwork = await SocialNetwork.deployed()
+  })
+
+  describe('deployment', async () => {
+    it('deploys successfully', async () => {
+      const address = await socialNetwork.address
+      assert.notEqual(address, 0x0)
+      assert.notEqual(address, '')
+      assert.notEqual(address, null)
+      assert.notEqual(address, undefined)
     })
 
-    describe('deployment', async () => {
-        it('deploys succesfully', async () => {
-            const address = await socialNetwork.address
-            assert.notEqual(address, 0x0)
-            assert.notEqual(address, '')
-            assert.notEqual(address, null)
-            assert.notEqual(address, undefined)
-        })
-
-        it('has a name', async () => {
-            const name = await socialNetwork.name()
-            assert.equal(name, 'Ethereum Social Network')
-        })
+    it('has a name', async () => {
+      const name = await socialNetwork.name()
+      assert.equal(name, 'Decentralized Social Network')
     })
-    describe('posts', async () => {
-        let result, postCount
-        it('create posts', async() => {
-            result = await socialNetwork.createPost('First Post in Ethereum Social Network', {from: author })
-            postCount = await socialNetwork.postCount()
-            // success 
-            assert.equal=(postCount, 1)
-        })
+  })
 
-        // it('lists posts', async() => {
+  describe('posts', async () => {
+    let result, postCount
 
-        // })
-        // it('allow users to tip posts', async() => {
-
-        // })
+    //before hooks for create posts
+    before(async () => {
+      result = await socialNetwork.createPost('This is my first post', { from: author })
+      postCount = await socialNetwork.postCount()
     })
+
+    it('creates posts', async () => {
+      // SUCESS
+      assert.equal(postCount, 1)
+      const event = result.logs[0].args
+      assert.equal(event.id.toNumber(), postCount.toNumber(), 'id is correct')
+      assert.equal(event.content, 'This is my first post', 'content is correct')
+      assert.equal(event.tipAmount, '0', 'tip amount is correct')
+      assert.equal(event.author, author, 'author is correct')
+
+      // FAILURE: Post must have content
+      await socialNetwork.createPost('', { from: author }).should.be.rejected;
+    })
+    it('lists posts', async () =>{
+      const post = await socialNetwork.posts(postCount)
+      assert.equal(post.id.toNumber(), postCount.toNumber(), 'id is correct')
+      assert.equal(post.content, 'This is my first post', 'content is correct')
+      assert.equal(post.tipAmount, '0', 'tip amount is correct')
+      assert.equal(post.author, author, 'author is correct')
+    })
+    it('allow users to tip posts', async()=>{
+      //Track author balance
+      let oldAuthorBalance
+      oldAuthorBalance = await web3.eth.getBalance(author)
+      oldAuthorBalance = new web3.utils.BN(oldAuthorBalance)
+
+      result = await socialNetwork.tipPost(postCount, { from: tipper, value: web3.utils.toWei('1', 'Ether') })
+
+      // SUCESS
+      assert.equal(postCount, 1)
+      const event = result.logs[0].args
+      assert.equal(event.id.toNumber(), postCount.toNumber(), 'id is correct')
+      assert.equal(event.content, 'This is my first post', 'content is correct')
+      assert.equal(event.tipAmount, '1000000000000000000', 'tip amount is correct')
+      assert.equal(event.author, author, 'author is correct')
+
+      //Check author received funds
+      let newAuthorBalance
+      newAuthorBalance = await web3.eth.getBalance(author)
+      newAuthorBalance = new web3.utils.BN(newAuthorBalance)
+
+      let tipAmount
+      tipAmount = web3.utils.toWei('1', 'Ether')
+      tipAmount = new web3.utils.BN(tipAmount)
+      
+      const expectedBalance = oldAuthorBalance.add(tipAmount)
+
+      assert.equal(newAuthorBalance.toString(), expectedBalance.toString())
+
+      //Failure: tries to tip a post that does not exist
+      await socialNetwork.tipPost(99, { from: tipper, value: web3.utils.toWei('1', 'Ether') }).should.be.rejected;
+    })
+    
+  })
 })
